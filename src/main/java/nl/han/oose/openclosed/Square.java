5ab0e448336bb0d9bbd1ad617a2b448cec81eb9e
Uwe van Heesch <uwe@vanheesch.net>
2018-09-20 10:26:54 +0200

    Square violating Liskov substitution

--- src/main/java/nl/han/oose/openclosed/Square.java
+++ src/main/java/nl/han/oose/openclosed/Square.java
@@ -6,7 +6,6 @@ public class Square extends Rectangle {
         super(width, width);
     }
 
-
     @Override
     public void setWidth(int value) {
         super.setWidth(value);
@@ -17,5 +16,16 @@ public class Square extends Rectangle {
     public void setHeight(int value) {
         super.setHeight(value);
         super.setWidth(value);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Square(2);
+
+        rectangle.setWidth(3);
+        rectangle.setHeight(2);
+
+        Rectangle rectangle2 = new Rectangle(2, 3);
+        rectangle2.setWidth(3);
+        rectangle2.setHeight(2);
     }
 }
